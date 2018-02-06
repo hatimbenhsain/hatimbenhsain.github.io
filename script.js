e12b4b7a5d8f96c67896ffbef832ef0ae3f9101d
hatimbenhsain <hb1487@nyu.edu>
2018-02-06 18:18:41 +0400

    upload all

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('Hello World!')
+let myName='hatim'
+console.log(myName)
+let myBlock=document.getElementById("myBlock")
+console.log(myBlock)
+myBlock.onclick=()=>{
+	console.log('i click\'d')
+}
