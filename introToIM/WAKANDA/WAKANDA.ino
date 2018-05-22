c24587a2b45035f6f783c2095987ac4d448ef594
hatimbenhsain <36029676+hatimbenhsain@users.noreply.github.com>
2018-05-22 22:31:01 +0400

    Add files via upload

---
+++ introToIM/WAKANDA/WAKANDA.ino
@@ -0,0 +1,24 @@
+int ledPin=13;
+
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPin,HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(200);
+  digitalWrite(ledPin,HIGH);
+  delay(100);
+  digitalWrite(ledPin,LOW);
+  delay(200);
+  digitalWrite(ledPin,HIGH);
+  delay(200);
+  digitalWrite(ledPin,LOW);
+  delay(100);
+  digitalWrite(ledPin,HIGH);
+  delay(200);
+}
