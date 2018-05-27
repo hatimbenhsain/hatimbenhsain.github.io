bf9d4b0587520654a4bfe1741f3d8b55d2bd98c8
hatimbenhsain <36029676+hatimbenhsain@users.noreply.github.com>
2018-05-28 02:47:38 +0400

    Add files via upload

---
+++ introToIM/drumstick/drumstick.ino
@@ -0,0 +1,32 @@
+#include <Servo.h>
+
+Servo myservo1;
+Servo myservo2;  
+
+int fsensor = A0;
+
+int pos = 0;    
+
+void setup() {
+  Serial.begin(9600);
+  myservo1.attach(3);  
+  myservo2.attach(5);
+  pinMode(fsensor, INPUT);
+}
+
+void loop() {
+
+  int fsensor_state = analogRead(fsensor);
+ Serial.println(fsensor_state);
+  if (fsensor_state >100){
+  for (pos = 0; pos <= 150; pos += 5) { 
+    // in steps of 1 degree
+    myservo1.write(pos);              
+    delay(15);                       
+  }
+  for (pos = 150; pos >= 0; pos -= 5) { 
+    myservo1.write(pos);
+                
+    delay(15);                       
+  }
+  }}
