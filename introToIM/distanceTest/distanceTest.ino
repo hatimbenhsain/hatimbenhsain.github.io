e80caebf27c4c98f49a7e8aa60a15734255a76de
hatimbenhsain <36029676+hatimbenhsain@users.noreply.github.com>
2018-05-28 02:32:17 +0400

    Add files via upload

---
+++ introToIM/distanceTest/distanceTest.ino
@@ -0,0 +1,107 @@
+/*
+SparkFun Inventor’s Kit
+Circuit 3B-Distance Sensor
+
+Control the color of an RGB LED using an ultrasonic distance sensor.
+
+This sketch was written by SparkFun Electronics, with lots of help from the Arduino community.
+This code is completely free for any use.
+
+View circuit diagram and instructions at: https://learn.sparkfun.com/tutorials/sparkfun-inventors-kit-experiment-guide---v40
+Download drawings and code at: https://github.com/sparkfun/SIK-Guide-Code
+*/
+#include "notes.h"
+
+int notes[]={NOTE_C3,NOTE_CS3,NOTE_D3,NOTE_DS3,NOTE_E3,NOTE_F3,NOTE_FS3,NOTE_G3,NOTE_GS3,NOTE_A3,NOTE_AS3,NOTE_B3,NOTE_C4,NOTE_CS4,NOTE_D4,NOTE_DS4,NOTE_E4,NOTE_F4,NOTE_FS4,NOTE_G4,NOTE_GS4,NOTE_A4,NOTE_AS4,NOTE_B4,NOTE_C5,NOTE_CS5,NOTE_D5,NOTE_DS5,NOTE_E5,NOTE_F5,NOTE_FS5,NOTE_G5,NOTE_GS5,NOTE_A5,NOTE_AS5,NOTE_B5,NOTE_CS6,NOTE_D6,NOTE_DS6,NOTE_E6,NOTE_F6,NOTE_FS6,NOTE_G6,NOTE_GS6,NOTE_A6,NOTE_AS6,NOTE_B6};
+int maxOctaves=4;
+int firstOctave=3;
+int place=0;
+
+const int trigPin = 11;           //connects to the echo pin on the distance sensor       
+const int echoPin = 12;           //connects to the trigger pin on the distance sensor      
+
+const int redPin = 3;             //pin to control the red LED inside the RGB LED
+const int greenPin = 5;           //pin to control the green LED inside the RGB LED
+const int bluePin = 6;            //pin to control the blue LED inside the RGB LED
+
+int potPin=A0;
+
+int maxDistance=4.55;
+
+int tonePin=13;
+int octave=0;
+int pot=0;
+
+float distance = 0;               //stores the distance measured by the distance sensor
+
+void setup()
+{
+  Serial.begin (9600);        //set up a serial connection with the computer
+
+  pinMode(trigPin, OUTPUT);   //the trigger pin will output pulses of electricity 
+  pinMode(echoPin, INPUT);    //the echo pin will measure the duration of pulses coming back from the distance sensor
+
+  //set the RGB LED pins to output
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(tonePin,OUTPUT);
+  pinMode(potPin,INPUT);
+
+}
+
+void loop() {
+  distance = getDistance();   //variable to store the distance measured by the sensor
+  pot=analogRead(potPin);
+  octave=map(pot,0,631,0,maxOctaves-1);
+  place=map(distance,1.5,4.55,0,11);
+  Serial.println(distance);
+  if(distance <= maxDistance){                         //if the object is close
+
+    //make the RGB LED red
+    analogWrite(redPin, 255);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 0);
+
+  } else if(maxDistance < distance && distance < 20){  //if the object is a medium distance
+
+    //make the RGB LED yellow
+    analogWrite(redPin, 255);
+    analogWrite(greenPin, 50);
+    analogWrite(bluePin, 0);
+
+  } else{                                     //if the object is far away
+
+    //make the RGB LED green
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 255);
+    analogWrite(bluePin, 0);    
+  }
+  if (distance<=10){
+    tone(tonePin,notes[place+octave*12],50);
+    
+  }
+  delay(50);      //delay 50ms between each reading
+}
+
+//------------------FUNCTIONS-------------------------------
+
+//RETURNS THE DISTANCE MEASURED BY THE HC-SR04 DISTANCE SENSOR
+float getDistance()
+{
+  float echoTime;                   //variable to store the time it takes for a ping to bounce off an object
+  float calcualtedDistance;         //variable to store the distance calculated from the echo time
+
+  //send out an ultrasonic pulse that's 10ms long
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+
+  echoTime = pulseIn(echoPin, HIGH);      //use the pulsein command to see how long it takes for the
+                                          //pulse to bounce back to the sensor
+
+  calcualtedDistance = echoTime / 148.0;  //calculate the distance of the object that reflected the pulse (half the bounce time multiplied by the speed of sound)
+
+  return calcualtedDistance;              //send back the distance that was calculated
+}
+
