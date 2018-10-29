0c4ba03f48d2f3f7a5014cdbd3624dd7990bb752
hatimbenhsain <36029676+hatimbenhsain@users.noreply.github.com>
2018-10-29 12:14:23 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,358 +1,233 @@
-var playerSide;
-var player;
-var box1;
-var myScale;
-var xPos;
-var yPos;
-var thic;
-var recU;
-var recB;
-var recL;
-var recR;
-var inrR;
-var outR;
-var hole;
-var spr;
-var holeSize;
-var shrinkRate;
-var blu;
-var holeX;
-var holeY;
-var holeW;
-var holeL;
-var speed;
-var moving;
-var isActive;
-var collision;
-var directions;
-var k;
-var score;
-var screen;
-var orgShrinkRate;
-var shrinkNum;
-var theFont;
-var txtSize;
-var pressS;
-var redR;
-var greR;
-var bluR;
-var redB;
-var greB;
-var bluB;
-var aValue;
-var testSprite;
-var redDif, bluDig, greDif;
-var playerUpScale;
-var highScore;
-var music;
-var deathSound;
-var unboxedSound;
-var noMusic;
+	let Y_AXIS=1
+	let X_AXIS=2
+	let b1, b2, c1, c2;
+	let upperX=0
+	let upperY=0
+	let	dsWidth=0
+	let dsHeight=0
+	let maxDSWidtch=1000
+	let angle=0
+	let k=0
+	let yeah=0
+	let up=document.getElementById('up')
+	let bottom=document.getElementById('bottom')
+	let test=''
+	let bottomFinalHeight=40
+	let bottomFinalTop=54
+	let bottomInitialTop=94
+	let bg=0
+	let width=0
+	let leftX=0
+	let screenUpWidth=0
+	let screenUpHeight=0
+	let screenBottomWidth=0
+	let screenBottomHeight=0
+	let bottomY=0
+	let middleY=0
+	let sideWidth=0
+	let dPadWidth=0
+	let dPadWidthMax=144
+	let dPadFat=50
+	let dPadLength=0
+	let dPadUp=0
+	let dPadBottom=0
+	let selected=0
+	let project1=document.getElementById('project1')
+	let project2=document.getElementById('project2')
+	let project3=document.getElementById('project3')
+	let project4=document.getElementById('project4')
+	let project5=document.getElementById('project5')
+	let project6=document.getElementById('project6')
+	let des1=document.getElementById('des1')
+	let des2=document.getElementById('des2')
+	let des3=document.getElementById('des3')
+	let des4=document.getElementById('des4')
+	let des5=document.getElementById('des5')
+	let des6=document.getElementById('des6')
+	let projects=[project1,project2,project3,project4,project5,project6]
+	let dess=[des1,des2,des3,des4,des5,des6]
+	let links=['./project1/index.html','./project2/index.html','./project3/comic.html','./project4/index.html','./project5/index.html','./goOut/index.html']
+	let gradient=0
+	let r=0
+	let y=0
+	let b=0
 
-class Box{
-	constructor(x,y,side){
-		this.x=x;
-		this.y=y;
-		this.side=side;
-		this.isActive=true;
-		var recU, recB, recL, recR;
-		speed=orgShrinkRate*((shrinkRate+shrinkNum)/(orgShrinkRate+shrinkNum));
-		this.speed=speed;
-		this.moving=false;
-		// this.recU=createSprite(x+xPos,y+yPos-boxWidth/2,boxWidth+thic,thic);
-		// this.recU.shapeColor=color(255);
-		// this.recU.velocity=createVector(0,speed);
-		// this.recB=createSprite(x+xPos,y+yPos+boxWidth/2,boxWidth+thic,thic);
-		// this.recB.shapeColor=color(255);
-		// this.recB.velocity=createVector(0,-speed);
-		// this.recL=createSprite(x+xPos-boxWidth/2,y+yPos,thic,boxWidth+thic);
-		// this.recL.shapeColor=color(255);
-		// this.recL.velocity=createVector(speed,0);
-		// this.recR=createSprite(x+xPos+boxWidth/2,y+yPos,thic,boxWidth+thic);
-		// this.recR.shapeColor=color(255);
-		// this.recR.velocity=createVector(-speed,0);
-		this.outR=createSprite(x+xPos,y+yPos,boxWidth,boxWidth);
-		this.outR.shapeColor=color(255);
-		this.inrR=createSprite(x+xPos,y+yPos,boxWidth-thic,boxWidth-thic);
-		this.inrR.shapeColor=blu;
-		this.getHole();
-		this.holeX=holeX;
-		this.holeY=holeY
-		this.hole=createSprite(x+xPos+this.holeX,y+yPos+this.holeY,holeW,holeL);
-		this.hole.shapeColor=blu;
-
-
+function Closed(){
+	this.setup=function(){
+		bg=loadImage("stars.gif")
+		gradient=loadImage('gradient.png')
+		createCanvas(windowWidth,windowHeight)
+		fullscreen(true)
+		upperY=windowHeight*0.52
+		upperX=(windowWidth-dsWidth)/2
+		bottom.style.top=(bottomInitialTop*windowHeight/100).toString()+'px'
+		bottom.style.height='0px'
+		r=color('#cc7083')
+		y=color('#cacc70')
+		b=color('#aaaaaa')
 	}
-	update(){
-		this.outR.scale=Math.pow(0.99,(shrinkRate+shrinkNum)/(orgShrinkRate+shrinkNum))*this.inrR.scale;
-		this.inrR.scale=Math.pow(0.99,(shrinkRate+shrinkNum)/(orgShrinkRate+shrinkNum))*this.outR.scale;
-		this.hole.scale=Math.pow(0.984,(shrinkRate+shrinkNum)/(orgShrinkRate+shrinkNum))*this.hole.scale;
-		this.holeX=this.holeX*Math.pow(0.99,(shrinkRate+shrinkNum)/(orgShrinkRate+shrinkNum));
-		this.holeY=this.holeY*Math.pow(0.99,(shrinkRate+shrinkNum)/(orgShrinkRate+shrinkNum));
-		//print(this.y+this.holeY+yPos);
-		if(this.moving==false){
-			this.hole.position=createVector(this.x+xPos+this.holeX,this.y+yPos+this.holeY);
+	this.draw=function(){
+		screenUpHeight=windowHeight*0.4
+		screenBottomHeight=windowHeight*0.4
+		dsHeight=windowHeight*0.44
+		bottomY=windowHeight*0.51
+		middleY=windowHeight*0.45
+		if (windowWidth>800){
+			screenBottomWidth=520
+			screenUpWidth=600
+		}else{
+			screenBottomWidth=0.65*windowWidth
+			screenUpWidth=0.75*windowWidth
 		}
-		if (!this.inrR.overlap(screen)){
-			this.isActive=false;
+		sideWidth=(dsWidth-screenBottomWidth)/2
+		dPadWidth=sideWidth*3/5
+		leftX=(windowWidth-dsWidth)/2
+		noCanvas()
+		createCanvas(windowWidth,windowHeight)
+		if (windowWidth<maxDSWidtch){
+			dsWidth=windowWidth*0.96
+		}else{
+			dsWidth=maxDSWidtch
 		}
-	}
-	getHole(){
-		if (this.side=="l"||this.side=="r"){
-			holeY=0;
-			holeW=boxWidth/2;
-			holeL=holeSize;
-			if(this.side=="l"){
-				holeX=-boxWidth/4;
-			}else{
-				holeX=boxWidth/4;
+		
+		fill('gray')
+		rect(leftX,bottomY,dsWidth,dsHeight,0,0,10,10)
+		b1=color('#aaaaaa')
+		b2=color('#727272')
+		setGradient(leftX,bottomY,dsWidth,dsHeight,b1,b2,Y_AXIS)
+		fill(b1)
+		rect(leftX,middleY,dsWidth,windowHeight*0.06,0,0,0,0)
+		setGradient(leftX,middleY,dsWidth,windowHeight*0.06,b1,b2,Y_AXIS)
+		rect(leftX,middleY,dsWidth*0.041,windowHeight*0.06,0,0,0,0)
+		rect(leftX+dsWidth*(1-0.041),middleY,dsWidth*0.041,windowHeight*0.06,0,0,0,0)
+		width=6*dsWidth/20-Number(bottom.style.height.slice(0,-2))/2
+		//rect(leftX+((dsWidth)-800)/4,windowHeight*0.51+(dsHeight/2-width/2),100,width/3)
+		fill(b1)
+		stroke('#686868')
+		strokeWeight(6)
+		if (windowWidth>800){
+			fill('#7094cc')
+			rect(leftX+sideWidth/5,bottomY+(dsHeight-dPadFat)/2,dPadWidth/3,dPadFat)
+			fill('#70cc7f')
+			rect(leftX+sideWidth/5+(dPadWidth-dPadFat)/2+dPadFat,bottomY+(dsHeight-dPadFat)/2,dPadWidth/3,dPadFat)
 			}
-		}else if(this.side=="u"||this.side=="b"){
-			holeX=0;
-			holeW=holeSize;
-			holeL=boxWidth/2
-			if(this.side=="u"){
-				holeY=-boxWidth/4
-			}else{
-				holeY=boxWidth/4
+		
+		fill(r)
+		dPadUp=rect(leftX+sideWidth/5+(dPadWidth-dPadFat)/2,bottomY+(dsHeight-dPadWidthMax)/2,dPadFat,dPadWidthMax/3)
+		fill(y)
+		dPadBottom=rect(leftX+sideWidth/5+(dPadWidth-dPadFat)/2,bottomY+(dsHeight-dPadFat)/2+dPadFat,dPadFat,dPadWidthMax/3)
+		fill(b)
+		ellipse(leftX+dsWidth-sideWidth/2,bottomY+dsHeight/2,100,100)
+		dPadUp
+		dPadBottom
+		fill('gray')
+		let bigRect=rect(upperX,upperY,dsWidth,dsHeight,angle,angle,10-angle,10-angle)
+		bigRect
+		setGradient(upperX,upperY,dsWidth,dsHeight,b1,b2,Y_AXIS)
+		let d1=dist(mouseX,mouseY,leftX+sideWidth/5+(dPadWidth-dPadFat)/2+dPadWidth/6,bottomY+(dsHeight-dPadFat)/2-dPadFat/4)
+		let d2=dist(mouseX,mouseY,leftX+sideWidth/5+(dPadWidth-dPadFat)/2+dPadFat/2,bottomY+(dsHeight-dPadFat)/2+dPadFat+dPadWidthMax/6)
+		let d3=dist(mouseX,mouseY,leftX+dsWidth-sideWidth/2,bottomY+dsHeight/2)
+		if ((d1<dPadWidthMax/5)||(d2<dPadWidthMax/6)||(d3<100/2)){
+			cursor(HAND)
+		}else{
+			cursor(ARROW)
+		}
+		if (d1<dPadWidthMax/5){
+			r='#ff214e'
+		}else{
+			r='#cc7083'
+		}
+		if (d2<dPadWidthMax/6){
+			y='#f1f44e'
+		}else{
+			y='#cacc70'
+		}
+		if(d3<100/2){
+			b='white'
+		}else{
+			b='#aaaaaa'
+		}
+		
+		this.open()
+	}
+	this.open=function(){
+		if ((upperY>windowHeight*0.02) && (yeah==0))
+		{
+			upperY-=windowHeight*0.01
+			upperX=leftX
+			angle+=10/60
+			if (Number(bottom.style.top.slice(0,-2))*100/windowHeight>bottomFinalTop){
+			bottom.style.top=(upperY+windowHeight*0.47).toString()+'px'
+			bottom.style.height=((bottomInitialTop*windowHeight/100)-(upperY+windowHeight*0.47)).toString()+'px'
 			}
+			else
+			{
+				bottom.style.top=(bottomFinalTop*windowHeight/100)+'px'
+				bottom.style.height=(bottomFinalHeight*windowHeight/100)+'px'
+			}	
 		}
-	}
-	go(dir){
-		this.moving=true;
-		unboxedSound.play();
-		//print(dir);
-		if(dir=="l"){
-			this.outR.setVelocity(this.speed,0);
-			this.inrR.setVelocity(this.speed,0);
-			this.hole.setVelocity(this.speed,0);
-			//print("left");
-		}else if(dir=="r"){
-			this.outR.setVelocity(-this.speed,0);
-			this.inrR.setVelocity(-this.speed,0);
-			this.hole.setVelocity(-this.speed,0);
-		}else if(dir=="u"){
-			this.outR.setVelocity(0,this.speed);
-			this.inrR.setVelocity(0,this.speed);
-			this.hole.setVelocity(0,this.speed);
-		}else if(dir=="b"){
-			this.outR.setVelocity(0,-this.speed);
-			this.inrR.setVelocity(0,-this.speed);
-			this.hole.setVelocity(0,-this.speed);
-		}
-
-	}
-}
-
-function setup(){
-	noMusic=true;
-	music=new sound("music.mp3");
-	deathSound=new sound("death.mp3");
-	unboxedSound=new sound("unboxed.mp3");
-	unboxedSound.volume=0.5;
-	music.loop=true;
-	playerUpScale=1.05;
-	aValue=0;
-	redB=60;
-	bluB=190;
-	greB=90;
-	pressS=0;
-	theFont=loadFont('https://hatimbenhsain.github.io/gamesNplay/unBoxing/8bitoperator.ttf');
-	textFont(theFont,width/10);
-	score=0;
-	textSize(txtSize);
-	shrinkNum=20;
-	directions=["l","r","u","b"];
-	collision=false;
-	blu=color(redB,greB,bluB);
-	createCanvas(245,245);
-	background(200);
-	shrinkRate=3;
-	orgShrinkRate=shrinkRate;
-	myScale=8;
-	playerSide=width/myScale;
-	boxWidth=height*2;
-	thic=boxWidth/4;
-	holeSize=boxWidth/4;
-	xPos=0;
-	yPos=0;
-	k=Math.floor(Math.random()*4);
-	screen=createSprite(width/2,height/2,width,height);
-	screen.shapeColor=blu;
-	box1=new Box(width/2,height/2,directions[k]);
-	player=createSprite(width/2,height/2,playerSide,playerSide);
-	player.shapeColor=color(255);
-	testSprite=createSprite(width/2,height/2,playerSide,playerSide);
-	testSprite.visible=false;
-	redR=250;
-	bluR=90;
-	greB=90;
-	if (typeof(Storage) !== "undefined") {
-    	if (localStorage.highScore!=null && localStorage.highScore!="undefined"){
-    		highScore=localStorage.highScore;
-    	}else{
-    		localStorage.highScore=0;
-    	}
-	} else {
-    // Sorry! No Web Storage support..
-    	highScore=0;
-	}
-	print(typeof(Storage));
-
-}
-
-function draw(){
-	//print(blu);
-	updateSprites();
-	background(blu);
-	drawSprites();
-	fill(255);
-	textFont(theFont);
-	textSize(width/4);
-	if(testSprite.removed==false && testSprite.width*testSprite.scale<width){
-		testSprite.scale=testSprite.scale*playerUpScale;
-		aValue++;
-		//print(aValue);
-		if (testSprite.width*testSprite.scale>=width){
-			testSprite.remove();
-			redDif=(redR-255)/aValue;
-			greDif=(greR-255)/aValue;
-			bluDif=(bluR-255)/aValue;			
-		}
-	}
-	if(score<10){
-		text(score,200,240);
-	}else{
-		text(score,165,240);
-	}
-	if (frameCount%32>15 && pressS<5*16 && collision==false){
-		textSize(14);
-		textAlign(CENTER);
-		text("press the arrows to\nescape the boxes",width/2,height*5/40);
-		textSize(50);
-		pressS++;
-	}
-	if (collision==false){
-		box1.update();
-	}else if(player.width*player.scale<width){
-		music.stop();
-		deathSound.play();	
-		player.scale=player.scale*playerUpScale;
-		player.shapeColor=color(redR,player.shapeColor.levels[1]+bluDif,player.shapeColor.levels[2]+bluDif);
-	}else if(player.width*player.scale>=width){
-		if (frameCount%64>32){
-			textSize(14);
-			textAlign(CENTER);
-			text("press the space bar\nto replay",width/2,height*5/40);
-		}
-		if (score>highScore){
-			highScore=score;
-		}
-		if (typeof(Storage) !== "undefined") {
-			localStorage.highScore=highScore;
-		}
-		textAlign(CENTER);
-		textSize(14);
-		text("high score:",width/2,height*15/40);
-		textSize(width/4);
-		text(highScore,width/2,height*26/40);
-	}
-	if (box1.isActive==false){
-		k=Math.floor(Math.random()*4);
-		box1.inrR.remove();
-		box1.outR.remove();
-		box1.hole.remove();
-		box1=new Box(width/2,height/2,directions[k]);
-		player.remove();
-		player=createSprite(width/2,height/2,playerSide,playerSide);
-		player.shapeColor=color(255);
-		score++;
-		if(shrinkRate<41){
-			shrinkRate++;
-		}
-		//print(shrinkRate);
-		//print(getBaseLog(orgShrinkRate,shrinkRate));
-
-	}
-	//print(box1.x);
-	checkOverlap();
-}
-function checkOverlap(){
-	// if ((player.overlap(box1.outR) && ((player.position.x-player.width/2)<=(box1.inrR.position.x-box1.inrR.width/2)
-	// 	||(player.position.x+player.width/2)>=(box1.inrR.position.x-box1.inrR.width/2) || 
-	// 	(player.position.y-player.height/2)<=(box1.inrR.position.y+box1.inrR.height/2) ||
-	// 	(player.position.y+player.height/2)>=(box1.inrR.position.x-box1.inrR.height/2)) 
-	// 	&& ((player.x-player.width/2)<=(box1.hole.x-box1.hole.width/2)||
-	// 	(player.x+player.width/2)>=(box1.hole.x+box1.hole.width/2) || (player.y-player.height/2)<=(box1.hole.y-box1.hole.height/2) ||
-	// 	/player.y+player.height/2)>=(box1.hole.x+box1.hole.height/2))
-	// 	){
-	// 		print('collision');
-	// 	}
-	if((player.overlap(box1.outR))&&(box1.inrR.width*box1.inrR.scale*5/8-Math.sqrt(Math.pow(player.position.x-box1.inrR.position.x,2)+
-		Math.pow(player.position.y-box1.inrR.position.y,2))-player.width<=0)&&
-		((player.overlap(box1.hole)&&player.width>=box1.hole.scale*holeSize)||!player.overlap(box1.hole))){
-		//print("collision");
-		collision=true;
-		box1.outR.setVelocity(0,0);
-		box1.hole.setVelocity(0,0);
-		box1.inrR.setVelocity(0,0);
-	}
-	//print(box1.inrR.width*box1.inrR.scale-Math.sqrt(Math.pow(player.position.x-box1.inrR.position.x,2)+
-	//	Math.pow(player.position.y-box1.inrR.position.y,2))-player.width,';',(player.overlap(box1.outR)));
-}
-function keyPressed(){
-	if (noMusic==true){
-		music.play();
-		noMusic=false;
-	}
-	if (keyCode==LEFT_ARROW||key=="a"){
-		if(box1.isActive==true&&box1.moving==false){
-			box1.go("l");
-		}
-	}else if(keyCode==RIGHT_ARROW||key=="d"){
-		if(box1.isActive==true&&box1.moving==false)
-			box1.go("r");
-	}else if(keyCode==UP_ARROW||key=="w"){
-		if(box1.isActive==true&&box1.moving==false)
-			box1.go("u");
-	}else if(keyCode==DOWN_ARROW||key=="s"){
-		if(box1.isActive==true&&box1.moving==false){
-			box1.go("b");
-		}
-	}
-	if(key==" "){
-		music.stop();
-		print("spaceBar");
-		if(collision==true){
-			print("replay");
-			setup();
+		else
+		{
+			upperY=windowHeight*0.02
+			upperX=leftX
+			angle=10
+			yeah=1
+			up.style.top=(windowHeight*0.06).toString()+"px"
+			up.style.opacity='1'
 		}
 	}
 }
+Closed()
+function mousePressed(){
+		let d1=dist(mouseX,mouseY,leftX+sideWidth/5+(dPadWidth-dPadFat)/2+dPadWidth/6,bottomY+(dsHeight-dPadFat)/2-dPadFat/4)
+		print(d1)
+		if (d1<dPadWidthMax/5){
+			print('ok')
+			if (k>0){
+				k-=1
+				projects[k].className='selected'
+				projects[k+1].className='project'
+				dess[k].className='des'
+				dess[k+1].className='desHidden'
+			}
+			bottom.scrollBy(0,-100)
 
-function getBaseLog(x, y) {
-  return Math.log(y) / Math.log(x);
+		}
+		let d2=dist(mouseX,mouseY,leftX+sideWidth/5+(dPadWidth-dPadFat)/2+dPadFat/2,bottomY+(dsHeight-dPadFat)/2+dPadFat+dPadWidthMax/6)
+		if(d2<dPadWidthMax/6){
+			print('cool')
+			if (k<5){
+				k+=1
+				projects[k].className='selected'
+				projects[k-1].className='project'
+				dess[k].className='des'
+				dess[k-1].className='desHidden'
+			}
+			bottom.scrollBy(0,100)
+		}
+		let d3=dist(mouseX,mouseY,leftX+dsWidth-sideWidth/2,bottomY+dsHeight/2)
+		if (d3<100/2){
+			window.location.href=links[k]
+		}
 }
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-        this.sound.play();
+function setGradient(x, y, w, h, c1, c2, axis) {
+
+  noFill();
+
+  if (axis == Y_AXIS) {  // Top to bottom gradient
+    for (var i = y; i <= y+h; i++) {
+      var inter = map(i, y, y+h, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x+w, i);
     }
-    this.stop = function(){
-        this.sound.pause();
+  }  
+  else if (axis == X_AXIS) {  // Left to right gradient
+    for (var i = x; i <= x+w; i++) {
+      var inter = map(i, x, x+w, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y+h);
     }
+  }
 }
-
-//DONE increase difficulty 
-//maybe add a box on top of the other box (box2)
-//DONE add score
-//DONE losing animation + replay
-//DONE music
-//add animation (pulse)
-//DONE highscore
-
