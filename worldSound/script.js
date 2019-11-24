71f759676501c7687904027c6d2451e947508b80
hatimbenhsain <36029676+hatimbenhsain@users.noreply.github.com>
2019-11-24 20:23:49 +0400

    Add files via upload

---
+++ worldSound/script.js
@@ -0,0 +1,130 @@
+let attackLevel = 1.0;
+let releaseLevel = 0;
+
+let attackTime = 0.001;
+let decayTime = 0.2;
+let susPercent = 0.2;
+let releaseTime = 0.5;
+
+let env, triOsc;
+
+let word="";
+
+let tempo;
+let bpm;
+let currentCount;
+let metFreq;
+let metHitFreq;
+let metronomeEnv;
+let metronomeHitEnv;
+
+let activated;
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+
+
+var t, t2;
+var synth;
+
+slider.oninput = function() {
+  tempo = this.value;
+  output.innerHTML = this.value;
+}
+
+function setup(){
+
+}
+
+document.onkeyup=function(e){
+	if (word==""){
+		createDiv("<p id=\"text\"></p>");
+	}
+	let t=select("text");
+	word=word+e.key;
+	document.getElementById("text").innerHTML=word;
+	if((e.keyCode>=65 && e.keyCode<=90)|| (e.keyCode>=97 && e.keyCode<=122)){
+		console.log(word);
+		env.play();
+		var audio = new Audio();
+  		audio.src =synth.Animalese(word,false).dataURI;
+  		audio.play();
+		//env.play();
+	}
+}
+
+function touchStarted() {
+	
+	// let cnv = createCanvas(100, 100);
+ //  textAlign(CENTER);
+ //  text('click to play', width/2, height/2);
+ 	output.innerHTML = slider.value;
+  env = new p5.Envelope();
+  env.setADSR(attackTime, decayTime, susPercent, releaseTime);
+  env.setRange(attackLevel, releaseLevel);
+
+  triOsc = new p5.Oscillator('triangle');
+  triOsc.amp(env);
+  triOsc.start();
+  triOsc.freq(150);
+  getAudioContext().resume();
+
+  console.log(activated);
+  if(activated!=true){
+  	tempo=80;
+	bpm=4;
+	metFreq=150;
+	currentCount=0;
+	activated=false;
+  	activated=true;
+  	synth = new Animalese('animalese.wav', function() {});
+  	beat();
+  }
+
+  activated=true;
+}
+
+function beat(){
+	if(currentCount==3){
+		env.mult(1.5);
+		env.play();
+		t2=setTimeout(function(){env.mult(1/1.5);},tempo*1000/120);
+	}
+	env.play();
+	currentCount++;
+	currentCount=currentCount%bpm;
+	t=setTimeout(beat,tempo*1000/120);
+}
+
+function draw(){
+
+}
+
+class Letter{
+	constructor(ch, count, time){
+		this.ch=ch;
+		this.count=count;
+		this.time=time;
+	}
+}
+
+
+function dataURItoBlob(dataURI) {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+    byteString = atob(dataURI.split(',')[1]);
+  else
+    byteString = unescape(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], {type:mimeString});
+}
