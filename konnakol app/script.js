803d6d7e66273a73909e3a0899e57e6162ebb144
hatimbenhsain <36029676+hatimbenhsain@users.noreply.github.com>
2021-06-09 20:33:57 +0400

    Add files via upload

--- konnakol app/script.js
+++ konnakol app/script.js
@@ -1,7 +1,7 @@
 fileNames=[];
 syllables=[];
 
-kala=60;	//tempo
+kala=80;	//tempo
 gati=4;		//akshara per beat
 
 $(document).ready(function () 
@@ -38,19 +38,22 @@ function GetSyllables(){
 		var s=fileNames.pop();
 		var sn=s.substr(0,s.length-1);
 		var added=false;
-		for(var i=0;i<syllables.length;i++){
-			if(syllables[i].name==sn){
-				syllables[i].sounds.push("sounds/"+s+".mp3");
-				added=true;
-				break;
+		if(isNumeric(s.substr(s.length-1,1))){
+			for(var i=0;i<syllables.length;i++){
+				if(syllables[i].name==sn){
+					syllables[i].sounds.push("sounds/"+s+".mp3");
+					added=true;
+					break;
+				}
 			}
-		}
-		if(!added){
-			syllables.push({
-				name:s.substr(0,s.length-1),
-				sounds:["sounds/"+s+".mp3"]
-			});
-			AddButton(sn);
+			if(!added){
+				syllables.push({
+					name:s.substr(0,s.length-1),
+					sounds:["sounds/"+s+".mp3"],
+					voice:"sounds/"+s.substr(0,s.length-1)+".mp3"
+				});
+				AddButton(sn);
+			}
 		}
 		GetSyllables();
 	}
@@ -66,6 +69,7 @@ function AddButton(sn){
 	but.className="seqButton";
 	but.addEventListener("click",function(){
 		textBox.value=textBox.value+this.id.slice(0,-6);
+		playVoice(this.id.slice(0,-6));
 	})
 }
 
@@ -99,10 +103,28 @@ function analyze(txt,ns){
 	if(txt!=""){
 		var s="";
 		var sn="";
-		for(var i=0;i<syllables.length;i++){
-			if(txt.length>=syllables[i].name.length && syllables[i].name.length>s.length && syllables[i].name==txt.substring(0,syllables[i].name.length)){
-				s=syllables[i];
-				sn=s.name;
+		if(txt.substr(0,5)=="tempo"){
+			s="tempo";
+			sn=s;
+		}else if(txt.substr(0,4)=="kala"){
+			s="kala";
+			sn=s;
+		}else if(txt.substr(0,1)=="g" || txt.substr(0,1)=="[" || txt.substr(0,1)=="]" || txt.substr(0,1)=="{" || txt.substr(0,1)=="}"){
+			s=txt.substr(0,1);
+			sn=s;
+		}else if(isNumeric(txt.substr(0,1))){
+			var l=1;
+			while(isNumeric(txt.substr(0,l+1)) && l<txt.length){
+				l+=1;
+			}
+			s=txt.substr(0,l);
+			sn=s;
+		}else{
+			for(var i=0;i<syllables.length;i++){
+				if(txt.length>=syllables[i].name.length && syllables[i].name.length>s.length && syllables[i].name==txt.substring(0,syllables[i].name.length)){
+					s=syllables[i];
+					sn=s.name;
+				}
 			}
 		}
 		if(s!=""){
@@ -116,14 +138,38 @@ function analyze(txt,ns){
 function playMusic(ns){
 	var delay=0
 	for(var i=0;i<ns.length;i++){
-		var fn=ns[i].sounds[Math.floor(Math.random()*ns[i].sounds.length)];
-		var t=delay+i*1000*(kala/60)/gati;
-		console.log("to play "+fn+" on "+t);
-		setTimeout(playSound,t,fn);
+		if(ns[i]=="tempo" || ns[i]=="kala"){
+			if(i+1<ns.length && isNumeric(ns[i+1])){
+				changeTempo(ns[i+1])
+				i++;
+			}
+		}else if(ns[i]=="g"){
+			if(i+1<ns.length && isNumeric(ns[i+1])){
+				changeGati(ns[i+1])
+				i++;
+			}
+		}else if(ns[i]=="[" || ns[i]=="}"){
+			changeTempo(kala*2);
+		}else if(ns[i]=="]" || ns[i]=="{"){
+			changeTempo(kala/2);
+		}else{
+			var fn=ns[i].sounds[Math.floor(Math.random()*ns[i].sounds.length)];
+			console.log("to play "+fn+" on "+delay);
+			setTimeout(playSound,delay,fn);
+			delay=delay+1000*(kala/60)/gati;
+		}
 	}
 	setTimeout(function(){
 		notePlayedDiv.style.visibility="hidden";
-	},delay+ns.length*1000*(kala/60)/gati)
+	},delay+1000*(kala/60)/gati)
+}
+
+function changeTempo(t){
+	kala=t;
+}
+
+function changeGati(g){
+	gati=g;
 }
 
 function playSound(fn){
@@ -136,4 +182,19 @@ function playSound(fn){
 function syllable(txt,sounds){
 	this.txt=txt;
 	get
+}
+
+function isNumeric(str) {			//function from https://stackoverflow.com/a/175787
+  if (typeof str != "string") return false // we only process strings!  
+  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+function playVoice(n){
+	for(var i=0;i<syllables.length;i++){
+		if(n==syllables[i].name){
+			playSound(syllables[i].voice);
+			break;
+		}
+	}
 }
