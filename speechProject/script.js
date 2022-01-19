54cb8d20b20152643735e12456f9a54cb50bb29a
hatim <hb1487@nyu.edu>
2022-01-19 16:50:55 +0400

    script v5

--- speechProject/script.js
+++ speechProject/script.js
@@ -27,12 +27,13 @@ let creds = {
 }
 
 class Node{
-	constructor(line){
+	constructor(line,words=[]){
 		this.line=line;
 		this.connections=[];
 		this.connectedWords=[];
 		this.forcedConnections=[];
 		nodeList.push(this);
+		this.wordsToConnect=words;
 	}
 	connect(connection){
 		this.connections.push(connection);
@@ -104,102 +105,155 @@ class Connection{
 	}
 }
 
-let node1=new Node("hala walla!");
-let node2=new Node("would you like to get to know me?");
-let node3=new Node("I would like to know you.");
-let node4=new Node("Hi, Are you ok?");
-let node5=new Node("I’m concerned about you");
-let node6=new Node("Where are you going?");
-let node7=new Node("Please stay!!!");
-let node8=new Node("How was your day?");
-let node9=new Node("What's your name?");
-let node10=new Node("My name is Jumairy");
-let node11=new Node("Can we please go outside?");
-let node12=new Node("Can i tell you a secret?");
-let node13=new Node("Do you love me?");
-let node14=new Node("Please tell me you love me!!!");
-let node15=new Node("LOVE ME");
-let node16=new Node("I’m BORED");
-let node17=new Node("HEY, Where do you live?");
-let node18=new Node("Wanna go for a ride?");
-let node19=new Node("Take me away with you");
-let node20=new Node("What are you doing tonight?");
-let node21=new Node("What are you doing today?");
-let node22=new Node("I love driving.");
-let node23=new Node("HEY, What’s your friend’s name?");
-let node24=new Node("Please help me!!!");
-let node25=new Node("Hey, I can’t live without you!!!");
-let node26=new Node("I worry a lot");
-let node27=new Node("I worry about you");
-let node28=new Node("Are you just going to leave me here?");
-let node29=new Node("At least say goodbye!!!");
-let node30=new Node("How can we get in touch?");
-let node31=new Node("I love you");
-let node32=new Node("I really like you ");
-let node33=new Node("Jumairy");
-let node34=new Node("Do you know Jumairy?");
-let node35=new Node("You’re beautiful");
-let node36=new Node("You’re gorgeous");
-let node37=new Node("We’re in heaven");
-let node38=new Node("Jumairy was here");
-let node39=new Node("I live in Jumeirah");
-let node40=new Node("Meet me on Jumeirah Road");
-let node41=new Node("Jumairy is my friend");
-let node42=new Node("Jumairy was here");
-let node43=new Node("because i love you");
-let node44=new Node("I come from Jumeirah");
-let node45=new Node("Jumairy told me to be here");
-let node46=new Node("I came to  spread love");
-let node47=new Node("nice to meet you");
-let node48=new Node("Can i get your number?");
+function ConnectAll(){
+	for(var i=0;i<nodeList.length;i++){
+		nodeList[i].childOfAll(nodeList[i].wordsToConnect);
+	}
+}
+
+let node1=new Node("hala walla!",[]);
+let node2=new Node("do you know jumairy",["hi"]);
+let node3=new Node("wha aleikum salam",["hello"]);
+let node4=new Node("I love jumairy",["I know ju","I know your mary","I know tu","I know jo"]);
+let node5=new Node("isn’t jumairy amazing?",["I do know ju","I do know tu","I do know jo","I do know your mary"]);
+let node6=new Node("i do too",["i do"]);
+let node7=new Node("ahhhhhhh i love jumairy",["who is ju","who is jo","who is tu","who is your mary"]);
+let node8=new Node("marhaba",["salam"]);
+let node9=new Node("it is a good morning indeed",["good morning"]);
+let node10=new Node("look at that smile",["good afternoon"]);
+let node11=new Node("evening? what time is it?",["good evening"]);
+let node12=new Node("my day is better that you’re with me now",["my day was good"]);
+let node13=new Node("it’s getting late… where are you going?",["good night"]);
+let node14=new Node("alhamdulilah rabi ya afeekum",["shehalik"]);
+let node15=new Node("i’m just a car in love",["what’s this"]);
+let node16=new Node("i’m in love with you",["who are you in love with","in love with"]);
+let node17=new Node("i love you",["who do you love"]);
+let node18=new Node("yes it’s you",["mirror mirror on the wall"]);
+let node19=new Node("cute",["zemanna"]);
+let node20=new Node("i’m here to share the love",["why are you here","why you here"]);
+let node21=new Node("i don’t know, i’m waiting for jumairy",["what’s happening"]);
+let node22=new Node("why? wanna catch up later?",["when are you leaving","when you leaving","when do you leave"]);
+let node23=new Node("give me yours first fdeitich",["your number"]);
+let node24=new Node("i’m ok habeebti",["you ok"]);
+let node25=new Node("I’m worried about you my love",["you worried"]);
+let node26=new Node("only when i think i might lose you",["you have anxiety"]);
+let node27=new Node("i really hope so",["you will never lose me"]);
+let node28=new Node("i’m happy now that you’re here",["you happy"]);
+let node29=new Node("never sad with you habeebti",["you sad"]);
+let node30=new Node("wouldn’t you want to know",["who is your"]);
+let node31=new Node("jumairy might see me talking to you",["worried about me"]);
+let node32=new Node("i’m afraid you won’t find true love after me",["why are you worried","why you worried"]);
+let node33=new Node("are you talking",["is it talking"]);
+let node34=new Node("no i’m speaking",["are you talking"]);
+let node35=new Node("where are you going",["let's go"]);
+let node36=new Node("i’m just a loving car",["what is this"]);
+let node37=new Node("you’re weird, i’m amazing",["weird"]);
+let node38=new Node("please stay",["have to go"]);
+let node39=new Node("my name is Jumairy",["your name"]);
+let node40=new Node("i want to run away with you",["where do you want to go"]);
+let node41=new Node("ask me to tell you a secret",["would you like to tell me something"]);
+let node42=new Node("jumairy is on his was here",["tell me a secret"]);
+let node43=new Node("i love you, i really really do",["do you love me"]);
+let node44=new Node("love me first",["love me"]);
+let node45=new Node("please tell me you love me!!!",["love"]);
+let node46=new Node("I’m bored too, where should we go",["i’m bored"]);
+let node47=new Node("wanna go for a ride?",["hey"]);
+let node48=new Node("take me away with you",["i want to leave"]);
+let node49=new Node("why? are you inviting me out?",["hobbies","hobby"]);
+let node50=new Node("I love driving",["activity","activity"]);
+let node51=new Node("can you call jumairy",["can i help"]);
+let node52=new Node("no. i can’t live without you habeebti",["can’t live without you"]);
+let node53=new Node("you too",["rude"]);
+let node54=new Node("i’ll go anywhere with you my love",["going tonight"]);
+let node55=new Node("We’re in heaven",["Can we go outside "]);
+let node56=new Node("Jumairy was here",["Can I tell you a secret"]);
+let node57=new Node("I love you",["Do you love me"]);
+let node58=new Node("I love you",["why are you bored"]);
+let node59=new Node("I live in Jumeirah",["where do you live"]);
+let node60=new Node("Meet me on Jumeirah Road",["wanna go for a ride"]);
+let node61=new Node("Meet me on Jumeirah Road",["doing tonight","what are you doing"]);
+let node62=new Node("Meet me on Jumeirah Road",["doing today "]);
+let node63=new Node("Jumairy is my friend",["your friend"]);
+let node64=new Node("because i love you",["concerned about me","worried about me"]);
+let node65=new Node("I come from Jumeirah",["come from","you from"]);
+let node66=new Node("i’m waiting for jumairy",["why are you here","why you here"]);
+let node67=new Node("i love that you’re here too",["do you like it here","do you like here"]);
+let node68=new Node("I worry a lot",["do you have anxiety"]);
+let node69=new Node("lets get married",["what can i do"]);
+let node70=new Node("I worry about you",["are you worried"]);
+let node71=new Node("Are you just going to leave me here?",["good bye","goodbye","good-bye"]);
+let node72=new Node("At least say goodbye!!!",["we have to leave"]);
+let node73=new Node("How can we get in touch?",["bye bye"]);
+let node74=new Node("byeeeeeeeee",["bye"]);
+let node75=new Node("nice to meet you fdeeitich",["see you"]);
+let node76=new Node("wish you can stay longer",["nice to meet you","nice meeting you"]);
+let node77=new Node("I love you",["I love you"]);
+let node78=new Node("I really like you",["I like you"]);
+let node79=new Node("Jumairy",["who owns you","owner"]);
+let node80=new Node("because Jumairy is not coming",["why do you worry","why are you worried"]);
+let node81=new Node("i don’t like food, but a cup of karak is always  a good choice",["like to eat","what food"]);
+let node82=new Node("aside from karak, i do like a bit of petrol",["like to drink","what drink"]);
+let node83=new Node("OH MY GOD, I LOVE HER",["who is Mu"]);
+let node84=new Node("Haha! you’re funny",["no"]);
+let node85=new Node("Great",["yes"]);
+let node86=new Node("i know",["you are beautiful","you're beautiful"]);
+let node87=new Node("thank you, i love you",["you're gorgeous","you are gorgeous"]);
+let node88=new Node("Mhmmmmmmmmmmm What’s your friend’s  name?",[]);
+let node89=new Node("What’s your name?",[]);
+let node90=new Node("Please help me!!!",[]);
+let node91=new Node("You’re beautiful",[]);
+let node92=new Node("You’re gorgeous",[]);
 
 randomNodes=[node1];
+randomNodes2=[node89,node88,node90,node91,node92]
 
-randomNodes2=[node11,node12,node13,node14,node15,node16,node17,node18,node19,node20,node21,
-node22,node23,node24,node25,node31,node9,node35,node36];
+ConnectAll();
+
+//randomNodes2=[node11,node12,node13,node14,node15,node16,node17,node18,node19,node20,node21,
+//node22,node23,node24,node25,node31,node9,node35,node36];
 
 
 //node1.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
-node2.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
-node2.forceConnection(node3);
-node4.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
-node5.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
-node34.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
-node48.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
-node4.forceConnection(node5);
-node6.childOfAll(["talkin","talking","what is this","weird"]);
-node6.forceConnection(node7);
-node8.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
-node4.forceConnection(node26);
-node4.forceConnection(node27);
-node26.childOfAll(["worried","worries","anxiety"]);
-node27.childOfAll(["worried","worries","anxiety"]);
-node28.childOfAll(["bye","goodbye","see you","later"]);
-node29.childOfAll(["have to leave","leaving","have to live"]);
-node30.childOfAll(["bye","goodbye","see you","later"]);
-node31.childOfAll(["love","you bored","you're bored"]);
-node32.childOfAll(["i like you"]);
-node34.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
-//node10.forceConnection(node33);
-node12.forceConnection(node33);
-node24.forceConnection(node33);
-node25.forceConnection(node33);
-node18.forceConnection(node33);
-node10.childOfAll(["your name","you're name"]);
-node11.makeConnection(node37,["yes","no"]);
-node13.makeConnection(node31,["yes","no"]);
-node12.makeConnection(node38,["yes","no"]);
-node39.childOfAll(["where do you live","where do you leave","you live where","were do you live","were do you leave"]);
-node18.makeConnection(node40,["yes","no"]);
-node40.childOfAll(["tonight","today","what are you doing"]);
-node41.childOfAll(["your friend","you're friend"]);
-node42.childOfAll(["why are you concerned","concerned"]);
-node43.childOfAll(["why are you concerned","concerned"]);
-node44.childOfAll(["where do you come","come from","where are you from","you from","from where"]);
-node45.childOfAll(["why are you here","why you here","why're you here"]);
-node46.childOfAll(["why are you here","why you here","why're you here"]);
-node47.childOfAll(["bye","goodbye","see you","later"]);
-node33.childOfAll(["your name","you're name","who are you","who're you","who owns","owner"]);
+// node2.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
+// node2.forceConnection(node3);
+// node4.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
+// node5.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
+// node34.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
+// node48.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
+// node4.forceConnection(node5);
+// node6.childOfAll(["talkin","talking","what is this","weird"]);
+// node6.forceConnection(node7);
+// node8.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
+// node4.forceConnection(node26);
+// node4.forceConnection(node27);
+// node26.childOfAll(["worried","worries","anxiety"]);
+// node27.childOfAll(["worried","worries","anxiety"]);
+// node28.childOfAll(["bye","goodbye","see you","later"]);
+// node29.childOfAll(["have to leave","leaving","have to live"]);
+// node30.childOfAll(["bye","goodbye","see you","later"]);
+// node31.childOfAll(["love","you bored","you're bored"]);
+// node32.childOfAll(["i like you"]);
+// node34.childOfAll(["salim","solemn","sanam","hi","hello","salam","alaikum","good morning","good evening","good afternoon","good after noon","sup","what's up"]);
+// //node10.forceConnection(node33);
+// node12.forceConnection(node33);
+// node24.forceConnection(node33);
+// node25.forceConnection(node33);
+// node18.forceConnection(node33);
+// node10.childOfAll(["your name","you're name"]);
+// node11.makeConnection(node37,["yes","no"]);
+// node13.makeConnection(node31,["yes","no"]);
+// node12.makeConnection(node38,["yes","no"]);
+// node39.childOfAll(["where do you live","where do you leave","you live where","were do you live","were do you leave"]);
+// node18.makeConnection(node40,["yes","no"]);
+// node40.childOfAll(["tonight","today","what are you doing"]);
+// node41.childOfAll(["your friend","you're friend"]);
+// node42.childOfAll(["why are you concerned","concerned"]);
+// node43.childOfAll(["why are you concerned","concerned"]);
+// node44.childOfAll(["where do you come","come from","where are you from","you from","from where"]);
+// node45.childOfAll(["why are you here","why you here","why're you here"]);
+// node46.childOfAll(["why are you here","why you here","why're you here"]);
+// node47.childOfAll(["bye","goodbye","see you","later"]);
+// node33.childOfAll(["your name","you're name","who are you","who're you","who owns","owner"]);
 
 // node1.makeConnection(node2,["hi","hello","sup"]);
 // node2.makeConnection(node3,["bad","not good","not"]);
