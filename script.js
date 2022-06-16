33c6186a3400edfd7921d0753bf6ab4de505fee8
hatim <hb1487@nyu.edu>
2022-06-17 01:22:12 +0300

    added comments

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // h1=h1.innerHTML;
 // var newh="";
 
-// for(var i=0;i<h1.length;i++){
+// for(var i=0;i<h1.length;i++){			//code to convert the HATIM BENHSAIN text into small <t> divisions
 // 	var c=h1.charAt(i)
 // 	if(c!="<" && c!="b" && c!="r" && c!=">" && c!=" "){
 // 		newh=newh+"<t>"+c+"</t>"
@@ -16,19 +16,19 @@
 
 var images=document.getElementsByTagName("img");
 
-for(var i=0;i<images.length;i++){
+for(var i=0;i<images.length;i++){		//making images not draggable for firefox and safari (this is handled in css for other browsers)
 	images[i].draggable=false;
 }
 
 var links=document.getElementsByTagName("a");
 
-for(var i=0;i<links.length;i++){
+for(var i=0;i<links.length;i++){		//same as above
 	links[i].draggable=false;
 }
 
 bgImage=document.getElementById("bgImage");
 
-function changeBg(d){
+function changeBg(d){					//changing the background hue periodically
 	console.log("change bg");
 	bgImage.style.filter="hue-rotate("+d+"deg)";
 	setTimeout(changeBg,200,d+1);
@@ -38,7 +38,7 @@ changeBg(313);
 
 var ts=document.getElementsByTagName("t");
 
-for(var i=0;i<ts.length;i++){
+for(var i=0;i<ts.length;i++){			//event listeners for the letters in the background, adding/removing a class when clicked 
 	ts[i].addEventListener("click",function(e){
 		if(e.which==1){
 			if(!e.target.classList.contains("tClicked")) e.target.classList.add("tClicked");
@@ -64,7 +64,7 @@ gallerySources=["janitoBot.jpg","kidloki.jpg","skeleguy.gif","amelMart.jpg","cha
 var clickableStuff=Array.from(document.getElementsByClassName("desktopIcon"));
 var desktopIcons=[];
 
-function mobileMode(){
+function mobileMode(){		//is the device on mobile?
 	return(window.matchMedia("(max-device-width: 640px) and (orientation: portrait)").matches ||
 		window.matchMedia("(max-device-height: 640px) and (orientation: landscape)").matches);
 }
@@ -77,7 +77,7 @@ var desktopIconInfo=[];
 
 var compStyle;
 
-for(var i=0;i<desktopIcons.length;i++){
+for(var i=0;i<desktopIcons.length;i++){		//data structure containing intial info for every icons such as their position
 	compStyle=window.getComputedStyle(desktopIcons[i]);
 	var info={id:desktopIcons[i].id,initLeft:compStyle.getPropertyValue('left'),initTop:compStyle.getPropertyValue('top'),
 		initRight:compStyle.getPropertyValue('right'),initBottom:compStyle.getPropertyValue('bottom')};
@@ -101,7 +101,7 @@ var galleryWindow=document.getElementById("galleryWindow");
 const galleryInfo={window:galleryWindow,initLeft:galleryWindow.style.left,initTop:galleryWindow.style.top};
 
 var infos=[aboutInfo,comicsInfo,gamesInfo,musicInfo,galleryInfo];
-for(var i=0;i<infos.length;i++){
+for(var i=0;i<infos.length;i++){		//info about windows including initial position and which icons they're linked to
 	infos[i].initRight=infos[i].window.style.right;
 	infos[i].initBottom=infos[i].window.style.bottom;
 }
@@ -125,7 +125,7 @@ var disX=0;
 var disY=0;
 
 
-const moveCursor = (e)=> {
+const moveCursor = (e)=> {			//what happens when cursor is moved
 	const mouseY = e.clientY;
 	const mouseX = e.clientX-64;
    
@@ -136,7 +136,7 @@ const moveCursor = (e)=> {
 		}
 	}
 
-	cursorNormal.style.top=mouseY+"px";
+	cursorNormal.style.top=mouseY+"px";		//moving fake cursor
 	cursorNormal.style.left=mouseX+"px";
 	cursorHover.style.top=mouseY+"px";
 	cursorHover.style.left=mouseX+"px";
@@ -148,29 +148,28 @@ const moveCursor = (e)=> {
 		if(crossClicked==-1){
 			if((mouseX+disX>0 && mouseY+disY>0 && mouseX+disX<screen.width*0.95 && mouseY+disY<screen.height*0.85)
 				&& (draggedElement.classList.contains("desktopIcon") || draggedElement.classList.contains("window"))){
-				draggedElement.style.left=mouseX+disX+"px";
+				draggedElement.style.left=mouseX+disX+"px";			//we move the dragged element with the mouse
 				draggedElement.style.top=mouseY+disY+"px";
 				draggedElement.style.bottom="auto";
 				draggedElement.style.right="auto";
-				// var rect = draggedElement.getBoundingClientRect();
+				// var rect = draggedElement.getBoundingClientRect();		// attempt to replace changing inset with translate, may work on it more latter
 				// var x=mouseX-rect.left-disX;
 				// var y=mouseY-rect.top-disY;
 				// draggedElement.style.transform="translate("+x+"px,"+y+"px)";
 			}
 
-			if(draggedElement.classList.contains("desktopIcon")){
+			if(draggedElement.classList.contains("desktopIcon")){		//animating trash icon if an item is hovering on top
 				if(collide(trashIcon.getElementsByClassName("iconImg")[0],draggedElement) && draggedElement.id!="trashIcon"){
 					trashIcon.getElementsByClassName("iconImg")[0].classList.add("trashOver");
 				}else{
 					trashIcon.getElementsByClassName("iconImg")[0].classList.remove("trashOver");
 				}
 			}
-			//console.log("moving");
-			//log.innerHTML=log.innerHTML+"<br>moving "+draggedElement.className;
+
 			var children=draggedElement.children;
 			if(mouseMoved){
 				for(var i=0;i<children.length;i++){
-					if(children[i].tagName.toLowerCase()=="a"){
+					if(children[i].tagName.toLowerCase()=="a"){		//cancelling links if the mouse is moved while clicking it
 						children[i].classList.remove("dragstart");
 			        	children[i].className=children[i].className + " dragstart";
 			        	log.innerHTML=log.innerHTML+"<br>removing a tag ";
@@ -182,7 +181,7 @@ const moveCursor = (e)=> {
 		}
 	}
 
-	if(mouseout){
+	if(mouseout){		//going back to normal cursor style
 		cursorNormal.style.visibility="visible";
 		cursorHover.style.visibility="hidden";
 		cursorClick.style.visibility="hidden";
@@ -195,18 +194,18 @@ window.addEventListener('mousemove', moveCursor)
 
 
 
-window.addEventListener('mousedown',function(e){
+window.addEventListener('mousedown',function(e){		//what happens when clicking mouse button
 	mouseMoved=false;
 	interacted=true;
 
 	mouseDownCoords[0]=e.clientX-64;
 	mouseDownCoords[1]=e.clientY;
 
-	if(!mobileMode() && interacted){
+	if(!mobileMode() && interacted){					//click sound only on desktop
 		clickSnd1.currentTime=0;
 		clickSnd1.play();
 	}
-	if(mouseHovering){
+	if(mouseHovering){									//changing cursor appearance
 		cursorNormal.style.visibility="hidden";
 		cursorHover.style.visibility="hidden";
 		cursorClick.style.visibility="visible";
@@ -214,23 +213,23 @@ window.addEventListener('mousedown',function(e){
 	mouseClicked=true;
 })
 
-window.addEventListener('mouseup',function(){
-	if(!mobileMode() && interacted){
+window.addEventListener('mouseup',function(){			//what happens when mouse click up
+	if(!mobileMode() && interacted){					//end click sound
 		clickSnd2.currentTime=0;
 		clickSnd2.play();
 	}
-	trashIcon.getElementsByClassName("iconImg")[0].classList.remove("trashOver");
+	trashIcon.getElementsByClassName("iconImg")[0].classList.remove("trashOver");		//resetting trash animation
 	var clicked=document.getElementsByClassName("clicked");
-	for(var i=0;i<clicked.length;i++){
+	for(var i=0;i<clicked.length;i++){				//removing clicked class from all objects
 		clicked[i].classList.remove("clicked");
 	}
 
-	var k = document.querySelectorAll(".dragstart")
+	var k = document.querySelectorAll(".dragstart")	//removing dragged class from all objects
     for(var i=0; i < k.length;i++){
         k[0].classList.remove("dragstart")
     }
 
-	if(mouseHovering){
+	if(mouseHovering){								//changing cursor img depending on if hovering on an object or not
 		cursorNormal.style.visibility="hidden";
 		cursorHover.style.visibility="visible";
 		cursorClick.style.visibility="hidden";
@@ -245,7 +244,7 @@ window.addEventListener('mouseup',function(){
 	
 })
 
-document.addEventListener("mouseleave",function(){
+document.addEventListener("mouseleave",function(){		//removing cursor image if the mouse is outside the page
 	cursorNormal.style.visibility="hidden";
 	cursorHover.style.visibility="hidden";
 	cursorClick.style.visibility="hidden";
@@ -263,8 +262,7 @@ for(var k=0;k<checkboxes.length;k++){
 var games=Array.from(document.getElementById("gamesWindow").getElementsByClassName("folderIcon"));
 
 for(var i=0;i<games.length;i++){
-	games[i].addEventListener("mouseenter",function(e){
-
+	games[i].addEventListener("mouseenter",function(e){			//highlighting the corresponding tags when a game is hovered
 		var tags=Array.from(e.target.classList);
 		tags.shift();
 		for(var n=0;n<tags.length;n++){
@@ -286,7 +284,7 @@ for(var i=0;i<games.length;i++){
 	});
 }
 
-function checkboxClicked(e){
+function checkboxClicked(e){			//when a checkbox is clicked, the list of games currently appearing is refreshed
 	var checkbox=e.target;
 	var tags=[];
 	for(var i=0;i<checkboxes.length;i++){
@@ -308,7 +306,7 @@ function checkboxClicked(e){
 				break;
 			}
 		}
-		if(containsTags){
+		if(containsTags){						//a game is displayed only if the corresponding tags apply to it
 			games[i].style.display="initial";
 		}else{
 			games[i].style.display="none";
@@ -316,7 +314,7 @@ function checkboxClicked(e){
 	}
 }
 
-function clearTags(e){
+function clearTags(e){							//reset games window when clicking clear button
 	var w=e.target.parentElement;
 	while(!w.classList.contains("windowContent") && w.parentElement!=null){
 		w=w.parentElement;
@@ -356,7 +354,7 @@ clickableStuff.push(document.getElementById("seekSlider"));
 
 for(var i=0;i<windows.length;i++){
 	windows[i]=windows[i].parentElement;
-	windows[i].addEventListener("click",function(e){
+	windows[i].addEventListener("click",function(e){				//put window on top of other windows when clicked
 		var w=e.target;
 		while(!w.classList.contains("window") && w.parentElement!=null){
 			w=w.parentElement;
@@ -369,12 +367,12 @@ var log=document.getElementById("log");
 
 var crosses=Array.from(document.getElementsByClassName("crossImg"));
 
-for(var i=0;i<crosses.length;i++){
+for(var i=0;i<crosses.length;i++){								
 	crosses[i].addEventListener("mousedown",function(e){
 		crossClicked=e.target;
 	});
 
-	crosses[i].addEventListener("mouseup",function(e){
+	crosses[i].addEventListener("mouseup",function(e){			//close window when cross is clicked (if there was no drag)
 		if(crossClicked==e.target && e.which==1){
 			var w=crossClicked;
 			while(!w.classList.contains("window") || w.classList.contains("windowBanner")){
@@ -392,7 +390,7 @@ for(var i=0;i<crosses.length;i++){
 	});
 }
 
-function replaceWindow(w){
+function replaceWindow(w){			//replace window to initial position when clicking corresponding icon
 	for(var i=0;i<infos.length;i++){
 		if(infos[i].window==w){
 			w.style.transition="0s";
@@ -425,7 +423,7 @@ for(var i=0;i<clickableStuff.length;i++){
 	clickableStuff[i].addEventListener('mouseup',clickableMouseUp);
 }
 
-function clickableMouseEnter(e){
+function clickableMouseEnter(e){			//playing sound when certain objects are hovered
 	var elem=e.target;
 
 	while(!elem.classList.contains("folderIcon") && !elem.classList.contains("desktopIcon")
@@ -442,7 +440,7 @@ function clickableMouseEnter(e){
 	}
 }
 
-function clickableMouseLeave(e){
+function clickableMouseLeave(e){		//handling cursor animation
 	if(!mouseClicked){
 		cursorNormal.style.visibility="visible";
 		cursorHover.style.visibility="hidden";
@@ -461,7 +459,7 @@ function clickableMouseMove(e){
 	mouseout=false;
 }
 
-function clickableMouseDown(e){
+function clickableMouseDown(e){			//function called when a clickable object is mouse down
 	if(e.which==1){
 		draggedElement=e.target.parentElement;
 		
@@ -472,10 +470,10 @@ function clickableMouseDown(e){
 			!draggedElement.classList.contains("checkbox") && !draggedElement.classList.contains("folderIcon") && 
 			!draggedElement.classList.contains("desktopIcon") && 
 			!draggedElement.classList.contains("window") && draggedElement.parentElement!=null){
-			draggedElement=draggedElement.parentElement;
+			draggedElement=draggedElement.parentElement;			//going back to the parent element in case this is an image or text etc
 		}
 
-		if(draggedElement.classList.contains("desktopIcon")){
+		if(draggedElement.classList.contains("desktopIcon")){		//put icon on top of other icons
 			putOnTop(draggedElement,desktopIcons,0);
 			draggedElement.getElementsByClassName("iconImg")[0].classList.add("clicked");
 		}
@@ -488,12 +486,12 @@ function clickableMouseDown(e){
 		}
 		const mouseY = e.clientY;
 		const mouseX = e.clientX-64;
-		disX=draggedElement.offsetLeft-mouseX;
+		disX=draggedElement.offsetLeft-mouseX;			//storing the distance between the mouse and the center of the element for drag displacement
 		disY=draggedElement.offsetTop-mouseY;
 	}
 }
 
-function clickableMouseUp(e){
+function clickableMouseUp(e){			//function called when a clickable object is mouse up
 	var elem=e.target;
 	if(e.which==1 && !elem.classList.contains("checkbox") && !elem.classList.contains("clearButton") &&
 		!draggedElement.classList.contains("buttonImg") &&
@@ -506,7 +504,7 @@ function clickableMouseUp(e){
 			if(elem.classList.contains("desktopIcon")){
 				var win=null;
 				var info=null;
-				switch(elem.id){
+				switch(elem.id){			//choose window depending on icon
 					case "aboutIcon":
 						info=aboutInfo;
 						break;
@@ -525,7 +523,7 @@ function clickableMouseUp(e){
 						break;
 				}
 				if(info!=null){
-					win=info.window;
+					win=info.window;				//open window
 					win.style.visibility="visible";
 					if(!mobileMode()){
 						win.style.top=info.initTop;
@@ -542,14 +540,14 @@ function clickableMouseUp(e){
 				}
 			}
 		}else if(elem==draggedElement && elem.classList.contains("desktopIcon")){
-			if(collide(trashIcon.getElementsByClassName("iconImg")[0],elem) && elem.id!="trashIcon"){
+			if(collide(trashIcon.getElementsByClassName("iconImg")[0],elem) && elem.id!="trashIcon"){		//put icon  in trash if hovering trash icon
 				putInTrash(elem);
 			}
 		}
 	}
 }
 
-function putOnTop(div, divs,minZ){
+function putOnTop(div, divs,minZ){				//puts div on top of divs with minZ as the minimum z the divs can reach
 	//console.log("put on top");
 	var ind=divs.indexOf(div);
 	divs.splice(ind,1);
@@ -566,7 +564,7 @@ var galleryNextButton=document.getElementById("galleryNextButton");
 var galleryPrevButton=document.getElementById("galleryPrevButton");
 var galleryImg=document.getElementById("galleryImage");
 
-galleryNextButton.addEventListener("click",function(){
+galleryNextButton.addEventListener("click",function(){			//handler for gallery buttons
 	currentImg++;
 	currentImg=((currentImg%gallerySources.length)+gallerySources.length)%gallerySources.length;
 	galleryImg.src="imgs/gallery/"+gallerySources[currentImg];
@@ -591,11 +589,11 @@ var nextButton=document.getElementById("nextButton");
 var prevButton=document.getElementById("prevButton");
 var muted=false;
 
-function loadMusicInfo(){
+function loadMusicInfo(){				//loads the music info when the music window is opened or the next/prev button is clicked
 	musicWindow.getElementsByClassName("bannerText")[0].innerHTML=songs[currentSong].id;
 	setSliderMax();
 	songs[currentSong].addEventListener('progress', displayBufferedAmount);
-	songs[currentSong].addEventListener('timeupdate', () => {
+	songs[currentSong].addEventListener('timeupdate', () => {		//reset slider
 		seekSlider.value = Math.floor(songs[currentSong].currentTime);
 		currentTimeContainer.textContent = calculateTime(seekSlider.value);
 	});
@@ -603,7 +601,7 @@ function loadMusicInfo(){
 	document.getElementById("albumCover").src=songs[currentSong].getElementsByClassName("albumCover")[0].src;
 }
 
-playButton.addEventListener("click",function(e){
+playButton.addEventListener("click",function(e){		//music play button handler
 	var img=e.target;
 	if(img.tagName!="IMG"){
 		img=img.getElementsByTagName("IMG")[0];
@@ -617,7 +615,7 @@ playButton.addEventListener("click",function(e){
 
 nextButton.addEventListener("click",nextSong);
 
-function nextSong(){
+function nextSong(){			//go to next song
 	var playing=musicPlaying;
 	stopMusic();
 	currentSong++;
@@ -628,7 +626,7 @@ function nextSong(){
 	}
 }
 
-function prevSong(){
+function prevSong(){		//go to prev song
 	var playing=musicPlaying;
 	stopMusic();
 	currentSong--;
@@ -639,8 +637,8 @@ function prevSong(){
 	}
 }
 
-prevButton.addEventListener("click",function(e){
-	if(songs[currentSong].currentTime>1){
+prevButton.addEventListener("click",function(e){		
+	if(songs[currentSong].currentTime>1){			//go to beginning of song if clicked prev while in the middle of a song
 		songs[currentSong].currentTime=0;
 	}else{
 		prevSong();
@@ -664,7 +662,7 @@ function stopMusic(){
 	songs[currentSong].currentTime=0;
 }
 
-muteButton.addEventListener("click",function(e){
+muteButton.addEventListener("click",function(e){			//mute button handler
 	var img=e.target;
 	if(img.tagName!="IMG"){
 		img=img.getElementsByTagName("IMG")[0];
@@ -679,37 +677,37 @@ muteButton.addEventListener("click",function(e){
 });
 
 
-function displayBufferedAmount(){
+function displayBufferedAmount(){			//show buffered amount on music slider
     const bufferedAmount = Math.floor(songs[currentSong].buffered.end(songs[currentSong].buffered.length - 1));
     audioPlayerContainer.style.setProperty('--buffered-width', `${(bufferedAmount/seekSlider.max) * 100}%`);
 }
 
-function setSliderMax(){
+function setSliderMax(){					//set music slider max according to song duration
 	seekSlider.max=Math.floor(songs[currentSong].duration);
 }
 
-function calculateTime(secs){
+function calculateTime(secs){				//calculate time to show on music player to 00:00 format
     const minutes = Math.floor(secs / 60);
     const seconds = Math.floor(secs % 60);
     const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
     return `${minutes}:${returnedSeconds}`;
 }
 
-seekSlider.addEventListener('input', () => {
+seekSlider.addEventListener('input', () => {			//music slider handler
   currentTimeContainer.textContent = calculateTime(seekSlider.value);
   songs[currentSong].currentTime = seekSlider.value;
 });
 
-function collide(el1, el2){
+function collide(el1, el2){						//checks if two elements are overlapping
 	var rect1 = el1.getBoundingClientRect();
 	var rect2 = el2.getBoundingClientRect();
 
 	return !(rect1.top > rect2.bottom || rect1.right < rect2.left || rect1.bottom < rect2.top || rect1.left > rect2.right);
 }
 
-function putInTrash(elem){
+function putInTrash(elem){						//puts icons in the trash
 	if(!elem.classList.contains("garbage")){
-		trash.push(elem);
+		trash.push(elem);			//if the icon is already garbage, it is not stored
 		elem.style.display="none";
 	}else{
 		var ind=desktopIcons.indexOf(elem);
@@ -722,8 +720,8 @@ function putInTrash(elem){
 var garbage=document.getElementsByClassName("garbage")[0];
 var garbageImgs=["garbage1.png","garbage2.png","garbage3.png"]
 
-function takeTrashOut(){
-	if(trash.length==0){
+function takeTrashOut(){		//clicking trash icon handler
+	if(trash.length==0){		//if trash is empty, a garbage icon is created and put at a random place.
 		var clone=garbage.cloneNode(true);
 		document.getElementsByClassName("windowIcons")[0].appendChild(clone);
 		clone.style.display="initial";
@@ -743,7 +741,7 @@ function takeTrashOut(){
 		setTimeout(removeTransition, 310,clone);
 		desktopIcons.push(clone);
 	}
-	else{
+	else{							
 		for(var i=0;i<trash.length;i++){
 			trash[i].style.display="block";
 			putOnTop(trash[i],desktopIcons,0);;
@@ -753,7 +751,7 @@ function takeTrashOut(){
 	trash=[];
 }
 
-function moveBack(elem){
+function moveBack(elem){		//moving icons back to original position after being taken out of trash
 	compStyle=window.getComputedStyle(trashIcon);
 	for(var i=0;i<desktopIconInfo.length;i++){
 		if(elem.id==desktopIconInfo[i].id){
@@ -775,6 +773,6 @@ function moveTo(elem,left,top){
 	console.log(elem);
 }
 
-function removeTransition(elem){
+function removeTransition(elem){		//removing transition from icon after moving it back
 	elem.style.transition="none";
 }
